fdc05c6c81894a6f1a4c55e47b1242164728a3de
yhorian <liam.jm.martin@gmail.com>
2022-12-14 22:08:04 +0000

    s

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -3,23 +3,24 @@
 // No extra work necessary.
 import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
-export const onRequest: PagesFunction = mailChannelsPlugin({personalizations: formEmailArray});
+export const onRequest: PagesFunction = mailChannelsPlugin({personalizations: emailPersonalizations, from: emailFrom, subject: emailSubject, content: emailContent, respondWith: responWithEmail });
 
-function formEmailArray(request, formdata, name) {
-    let personalizations = { personalizations:[
-      {
-        to: [{ name: "ACME Support", email: "support@example.com" }],
-      },
-    ],
-    from: {
-      name: "ACME Support",
-      email: "support@example.com",
-    },
-    respondWith: () => {
-      return new Response(
-        `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
-      );
-    },
-  }
-  return personalizations
+function emailPersonalizations(request, formdata, name) {
+  return [{to: [{ name: "ACME Support", email: "support@example.com" }]}]
+}
+
+function emailFrom(request, formData, name) {
+  return {name: "ACME Support", email: "support@example.com"}
+}
+
+function responWithEmail(request, formData, name) {
+  return new Response(`Thank you for submitting your enquiry. A member of the team will be in touch shortly.`)
+}
+
+function emailContent(request, formData, name) {
+  return ["test content"]
+}
+
+function emailSubject(request, formData, name) {
+  return "Test Subject 3382"
 }
