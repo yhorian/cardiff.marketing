cb35f60724ac4a2c0d79c53416f50424c382bc32
yhorian <liam.jm.martin@gmail.com>
2022-12-15 00:15:53 +0000

    s

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,11 +1,10 @@
 // Implements this plugin: https://developers.cloudflare.com/pages/platform/functions/plugins/mailchannels/
 // Will capture anything from a form with a 'data-static-form-name' attribute, for example: <form data-static-form-name="contact">
 // No extra work necessary.
-// replace this with your details:
+// Replace this with your details:
 const myEmail = "info@cardiff.marketing"
 
 import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
-import { data } from "autoprefixer";
 
 export const onRequest: PagesFunction = mailChannelsPlugin({personalizations: emailPersonalizations, from: emailFrom, subject: emailSubject,  respondWith: () =>
   new Response(null, {
