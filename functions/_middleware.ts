a3d3e63daf55781a52b171fd52bef3df42645f7d
yhorian <liam.jm.martin@gmail.com>
2022-12-15 08:58:59 +0000

    s

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -6,6 +6,8 @@ const myEmail = "info@cardiff.marketing"
 
 import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
+// Expects object literal with properties for: "Personalizations" with a 'to' field, "from": with name + email, "respondWith" as a response or promise of a response object.
+// Optional Properties: subject, content. Define the subject/content for the email.
 export const onRequest: PagesFunction = mailChannelsPlugin({personalizations: emailPersonalizations, from: emailFrom, subject: emailSubject,  respondWith: formResponse,});
 
 function emailPersonalizations() {
@@ -17,9 +19,25 @@ function emailFrom(data) {
 }
 
 function formResponse() {
-  return new Response(null, {status: 302, headers: {Location: "about/"} })
+  return new Response("whoops", {status: 500, headers: {Location: "https://cardiff.marketing/about/"} })
 }
 
 function emailSubject(data) {
   return `${ data.name } form submission from: ` + data.formData.get("email")
+}
+
+// This is the default template you get if 'content' isn't set, I've just extracted it to make it easier to understand and edit.
+function emailContent(data) {
+  return `<!DOCTYPE html>
+  <html>
+    <body>
+      <h1>New contact form submission</h1>
+      <div>At ${new Date().toISOString()}, you received a new ${data.name} form submission from ${data.request.headers.get("CF-Connecting-IP")}:</div>
+      <table>
+      <tbody>
+      ${[...data.formData.entries()].map(([field, value]) => `<tr><td><strong>${field}</strong></td><td>${value}</td></tr>`).join("\n")}
+      </tbody>
+      </table>
+    </body>
+  </html>`;
 }
