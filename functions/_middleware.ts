179eee6ac2b8d3db11b9b400abb7b8247d40122e
yhorian <liam.jm.martin@gmail.com>
2022-12-15 00:10:46 +0000

    s

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,20 +1,23 @@
 // Implements this plugin: https://developers.cloudflare.com/pages/platform/functions/plugins/mailchannels/
 // Will capture anything from a form with a 'data-static-form-name' attribute, for example: <form data-static-form-name="contact">
 // No extra work necessary.
+// replace this with your email:
+const myEmail = "info@cardiff.marketing"
 import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
 export const onRequest: PagesFunction = mailChannelsPlugin({personalizations: emailPersonalizations, from: emailFrom, subject: emailSubject,  respondWith: () =>
   new Response(null, {
     status: 302,
-    headers: { Location: "/thank-you" },
-  }), content: emailContent });
+    headers: { Location: "/about" },
+  })
+});
 
 function emailPersonalizations(data) {
-  return [{to: [{ name: "Info", email: "info@cardiff.marketing" }],},]
+  return [{to: [{ name: "Me", email: myEmail }],},]
 }
 
 function emailFrom(data) {
-  return {name: data.formData.get('name'), email: "enquiries@cardiff.marketing"}
+  return {name: "Form Submission", email: myEmail}
 }
 
 function responWithEmail(data) {
@@ -23,8 +26,4 @@ function responWithEmail(data) {
 
 function emailSubject() {
   return "Test Subject " + Math.random()
-}
-
-function emailContent(data) {
-  return [{type: "text/plain", value:"Message from " + data.formData.get('email') + ": " + data.formData.get('message'),},]
 }
