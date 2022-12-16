fed3225f13d8024fabbd1dbd473ef10746c8212d
yhorian <liam.jm.martin@gmail.com>
2022-12-16 14:52:15 +0000

    s

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,6 +1,6 @@
 import staticFormsPlugin from "pages-plugin-mailchannels-bugfix";
 
-export const onRequest: PagesFunction = pages-plugin-mailchannels-bugfix({
+export const onRequest: PagesFunction = staticFormsPlugin({
   respondWith: ({ formData, name }) => {
     const email = formData.get('email')
     return Response.redirect('https://cardiff.marketing/about', 302)
--- mailchannel/index.d.ts
+++ mailchannel/index.d.ts
@@ -1,23 +1,11 @@
-import type { MailSendBody } from "./api";
-
-interface Submission {
-  request: Request;
-  formData: FormData;
-  name: string;
-}
-
 export type PluginArgs = {
-  personalizations:
-    | MailSendBody["personalizations"]
-    | ((submission: Submission) => MailSendBody["personalizations"]);
-  from:
-    | MailSendBody["from"]
-    | ((submission: Submission) => MailSendBody["from"]);
-  subject?:
-    | MailSendBody["subject"]
-    | ((submission: Submission) => MailSendBody["subject"]);
-  content?: (submission: Submission) => MailSendBody["content"];
-  respondWith: (submission: Submission) => Response | Promise<Response>;
+  respondWith: ({
+    formData,
+    name,
+  }: {
+    formData: FormData;
+    name: string;
+  }) => Response | Promise<Response>;
 };
 
 export default function (args: PluginArgs): PagesFunction;
