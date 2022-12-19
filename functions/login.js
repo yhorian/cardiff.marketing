2e1ef3b915b4b434ade50f093e3569c985eb8862
yhorian <liam.jm.martin@gmail.com>
2022-12-19 09:38:02 +0000

    s

--- functions/login.js
+++ functions/login.js
@@ -1,71 +1,64 @@
-export function onRequest(context) {
-    return Response.redirect("https://cardiff.marketing/about/", 302)
-  }
-  addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-  })
-  
-  async function handleRequest(request) {
-    // Parse the query string of the request
-    const params = new URL(request.url).searchParams
-    const state = params.get('state')
-    const code = params.get('code')
-  
-    // Check if the request is the redirect from the GitHub OAuth flow
-    if (state && code) {
-      // Exchange the code for an access token
-      const tokenResponse = await exchangeCodeForToken(code)
-      const accessToken = tokenResponse.access_token
-  
-      // Use the access token to retrieve the user's profile
-      const userResponse = await getUserProfile(accessToken)
-      const user = userResponse.data
-  
-      // Set a cookie with the user's information to complete the login flow
-      const cookie = `user=${JSON.stringify({
-        avatar_url: user.avatar_url,
-        name: user.name,
-      })}`
-      const response = new Response('', {
-        headers: {
-          'Set-Cookie': cookie,
-        },
-      })
-  
-      // Redirect the user back to the NetlifyCMS page
-      return response.redirect(state)
-    } else {
-      // Otherwise, return the default response
-      return new Response('Hello World!')
-    }
-  }
-  
-  async function exchangeCodeForToken(code) {
-    // Replace GITHUB_CLIENT_ID and GITHUB_CLIENT_SECRET with your own values
-    const clientId = 'GITHUB_CLIENT_ID'
-    const clientSecret = 'GITHUB_CLIENT_SECRET'
-  
-    const response = await fetch('https://github.com/login/oauth/access_token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify({
-        client_id: clientId,
-        client_secret: clientSecret,
-        code,
-      }),
-    })
-    return await response.json()
-  }
-  
-  async function getUserProfile(accessToken) {
-    const response = await fetch('https://api.github.com/user', {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    })
-    return await response.json()
-  }
-  
+export async function onRequest({ request, next, env }) {
+		// Parse the query string of the request
+		const params = new URL(request.url).searchParams
+		const state = params.get('state')
+		const code = params.get('code')
+
+		// Check if the request is the redirect from the GitHub OAuth flow
+		if (state && code) {
+			// Exchange the code for an access token
+			const tokenResponse = await exchangeCodeForToken(code)
+			const accessToken = tokenResponse.access_token
+
+			// Use the access token to retrieve the user's profile
+			const userResponse = await getUserProfile(accessToken)
+			const user = userResponse.data
+
+			// Set a cookie with the user's information to complete the login flow
+			const cookie = `user=${JSON.stringify({
+				avatar_url: user.avatar_url,
+				name: user.name,
+			})}`
+			const response = new Response('', {
+				headers: {
+					'Set-Cookie': cookie,
+				},
+			})
+
+			// Redirect the user back to the NetlifyCMS page
+			return response.redirect(state)
+		} else {
+			// Otherwise, return the default response
+			return new Response('Hello World! Nothing to see here.')
+		}
+	};
+
+
+async function exchangeCodeForToken(code) {
+	// Replace GITHUB_CLIENT_ID and GITHUB_CLIENT_SECRET with your own values
+	const clientId = '97255fe0b08af260b1a2'
+	const clientSecret = '857b7f02b2e54fdeac503901eb5575c1d1aa8cdc'
+
+	const response = await fetch('https://github.com/login/oauth/access_token', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: 'application/json',
+		},
+		body: JSON.stringify({
+			client_id: clientId,
+			client_secret: clientSecret,
+			code,
+		}),
+	})
+	return await response.json()
+}
+
+async function getUserProfile(accessToken) {
+	const response = await fetch('https://api.github.com/user', {
+		headers: {
+			Authorization: `Bearer ${accessToken}`,
+		},
+	})
+	return await response.json()
+}
