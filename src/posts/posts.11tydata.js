387720fd07114705350913562830460f3e87b5a5
yhorian <liam.jm.martin@gmail.com>
2022-12-23 19:10:32 +0000

    removing cache from git

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -1,7 +1,7 @@
 module.exports = {
   eleventyComputed: {
   "author": data => (data.author) ? data.author : data.settings.author,
-  "permalink": data => (data.permalink) ? data.permalink : filters.slugify(data.title) + "/",
+  "permalink": data => (data.permalink) ? data.permalink : filters.slugify(data.title),
     },
   "layout": "post.pug",
   "tags": ["post"],
