4912efc5c9bf9ef0e21e0dd398bc9792dd3c661a
yhorian <liam.jm.martin@gmail.com>
2023-01-08 16:38:55 +0000

    optimising the build process

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const Image = require("@11ty/eleventy-img");
-const fs = require('fs');
 const emojiReadTime = require("@11tyrocks/eleventy-plugin-emoji-readtime");
 const markdownIt = require('markdown-it');
 const markdownItAnchor = require('markdown-it-anchor');
@@ -10,9 +9,6 @@ const markdownItToC = require("markdown-it-toc-done-right")
 const PostCSSPlugin = require("eleventy-plugin-postcss");
 const postcssrc = require('postcss-load-config')
 const criticalCss = require("eleventy-critical-css");
-const { postcss } = require("postcss-preset-env");
-
-var favicons = "";
 
 console.log(`Running as ${process.env.NODE_ENV}.`);
 
@@ -21,17 +17,12 @@ var plugins, options = postcssrc({
   map: 'inline'
 })
 
-async function getFavicons() {
-  let result = await favGen("./src/static/img/cm-icon.png", "./_site", {
+function getFavicons() {
+  favGen("./src/static/img/cm-icon.png", "./_site", {
     manifestData: {},
     generateManifest: false,
     skipCache: false
-  }).then(
-    (result) => {
-      return favHtml(result)
-    }
-  );
-  favicons = result;
+  })
 }
 
 function articleImageProcess({
@@ -46,59 +37,8 @@ function articleImageProcess({
   height = false,
   width = false
 }) {
-  // Not waiting for the image stats, just go!
-  Image(src, {
-    widths: widths,
-    formats: formats,
-    urlPath: "/static/img/",
-    outputDir: "./_site/static/img/"
-  });
-
-  // Get image stats from the file synchonously. Images can process in the background.
-  let metadata = Image.statsSync(src, {
-    widths: widths,
-    formats: formats,
-    urlPath: "/static/img/",
-    outputDir: "./_site/static/img/",
-  });
-  let lowsrc = metadata.webp[0];
-  let highsrc = metadata.webp[metadata.webp.length - 1];
-
-  // Build the img element
-  let img = `<img
-    src="${lowsrc.url}"
-    width="${(width) ? width : highsrc.width}"
-    height="${(height) ? height : highsrc.height}"
-    alt="${alt}"
-    ${(lazy) ? 'loading="lazy"' : ''}
-    ${(_class) ? 'class="' + _class + '"' : ''}
-    decoding="async">`;
-
-  // Build the picture element
-  let picture = `<picture> ${Object.values(metadata).map(imageFormat => {
-    return imageFormat.map(image => {
-      return `<source type="${image.sourceType}" srcset="${image.srcset}" sizes="${sizes}">`;
-    }).join("\n");
-  }).join("\n")}${img}</picture>`;
-
-  // Build the figure element
-  let figure = `<figure>${picture}${(label) ? '<figcaption>' + alt + '</figcaption>' : ''}</figure>`;
-
-  return figure;
-}
-
-function lazyImagineProcess({
-  src,
-  alt = "",
-  label = false,
-  _class = false,
-  sizes = "(max-width: 1000px) 800px, 1200px",
-  widths = ["auto", "800", "1200"],
-  formats = ["webp", "png"],
-  lazy = true,
-  height = false,
-  width = false
-}) {
+  // Lazy return method for production that does no processing.
+  if (process.env.NODE_ENV == "prod") {
   return `<figure><picture> 
       <img
         src="${src.replace("src", "")}"
@@ -109,7 +49,46 @@ function lazyImagineProcess({
         ${(_class) ? 'class="' + _class + '"' : ''}decoding="async"> </picture> 
         ${(label) ? '<figcaption>'+ alt + '</figcaption>' : ''}
         </figure>`
+  }
+
+  // Not waiting for the image stats, just go! Pug can't do asynchonous calls.
+  Image(src, {
+    widths: widths,
+    formats: formats,
+    urlPath: "/static/img/",
+    outputDir: "./_site/static/img/"
+  });
+
+  // Get image stats from the file synchonously. Images process in the background.
+  let metadata = Image.statsSync(src, {
+    widths: widths,
+    formats: formats,
+    urlPath: "/static/img/",
+    outputDir: "./_site/static/img/",
+  });
+  let lowsrc = metadata.webp[0];
+  let highsrc = metadata.webp[metadata.webp.length - 1];
+
+  let img = `<img
+    src="${lowsrc.url}"
+    width="${(width) ? width : highsrc.width}"
+    height="${(height) ? height : highsrc.height}"
+    alt="${alt}"
+    ${(lazy) ? 'loading="lazy"' : ''}
+    ${(_class) ? 'class="' + _class + '"' : ''}
+    decoding="async">`;
+
+  let picture = `<picture> ${Object.values(metadata).map(imageFormat => {
+    return imageFormat.map(image => {
+      return `<source type="${image.sourceType}" srcset="${image.srcset}" sizes="${sizes}">`;
+    }).join("\n");
+  }).join("\n")}${img}</picture>`;
+
+  let figure = `<figure>${picture}${(label) ? '<figcaption>' + alt + '</figcaption>' : ''}</figure>`;
+
+  return figure;
 }
+
 // Set up markdown-it instance with Anchor/Table of Contents plugin
 const markdownLib = markdownIt({
   html: true,
@@ -143,6 +122,7 @@ module.exports = (eleventyConfig) => {
   if (process.env.NODE_ENV == "prod") {
     eleventyConfig.on('eleventy.before', getFavicons);
   }
+
   // Less terminal output
   eleventyConfig.setQuietMode(true);
 
@@ -154,7 +134,7 @@ module.exports = (eleventyConfig) => {
 
   // Filter to add favicon data
   eleventyConfig.addFilter("favicons", () => {
-    return favicons;
+    return favicons
   });
 
   // Merge data instead of overriding
@@ -187,16 +167,20 @@ module.exports = (eleventyConfig) => {
     debug: false
   });
 
+  // Postcss plugin for processing and minifying tailwind.
   eleventyConfig.addPlugin(PostCSSPlugin, plugins, options);
 
+  // Image processing plugin.
+  eleventyConfig.addFilter('image', articleImageProcess);
+
+  // Inline critical CSS if on production. CSS is so small, this can be removed and all CSS inlined anyway.
+  // Currently sets a listener for each page and gets a bit silly. Warning disabled as a FIX.
   if (process.env.NODE_ENV == "prod") {
-    eleventyConfig.addFilter('image', articleImageProcess);
+    process.setMaxListeners(0);
     eleventyConfig.addPlugin(criticalCss, {
       height: 1080,
       width: 1920,
     });
-  } else {
-    eleventyConfig.addFilter('image', lazyImagineProcess);
   }
 
   // Markdown files will be run through the nunjucks parser. Lets us embed {% nunjuck code %}.
